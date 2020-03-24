d7803052c90338c191e39cf2362f0d260e5d7f3a
thaole25 <thao.t.le25@gmail.com>
2020-03-24 16:07:22 +0700

    fixed forecast model: increasing numbers

--- main.py
+++ main.py
@@ -2,37 +2,54 @@ import math
 import pandas as pd
 import numpy as np
 
+DATA_FOLDER = '' #'../input/covid19-global-forecasting-week-1/'
+
 def read_data_training():
-  df_train = pd.read_csv('train.csv', index_col='Id')
+  df_train = pd.read_csv(DATA_FOLDER + 'train.csv')
   df_train['Date'] = pd.to_datetime(df_train['Date'], format='%Y-%m-%d')
   return df_train
 
+def print_all_rows(df):
+  pd.set_option('display.max_rows', df.shape[0]+1)
+  print(df)
+    
 def prophet_calculation(df_train, label):
   from fbprophet import Prophet
   selected_df_train = df_train[['Date', label]]
+  selected_df_train.reset_index(inplace=True)
   selected_df_train.rename(columns={'Date': 'ds'}, inplace=True)
-  selected_df_train.rename(columns={label: 'y'}, inplace=True)
+
+  # difference between two consecutive days
+  selected_df_train['y'] = selected_df_train[label] - selected_df_train[label].shift(1)
+  selected_df_train.loc[0, 'y'] = 0
+  # print_all_rows(selected_df_train)
+
   # Train the model
   model = Prophet(growth='linear')
   model.fit(selected_df_train)
+
   # Predict the future
   future_df = model.make_future_dataframe(periods=50)
   forecast = model.predict(future_df)
-  forecast.index = range(1, len(future_df) + 1)
-  forecast = forecast[['ds', 'yhat']]
-  forecast['y'] = selected_df_train['y']
   forecast['yhat'] = pd.to_numeric(forecast['yhat'])
   forecast.loc[forecast['yhat'] < 0, ['yhat']] = 0
-  forecast = forecast.round({'yhat': 0})
+  # Ensure that 'yhat' is always increasing
+  for i in range(1, len(forecast)):
+    if forecast.loc[i - 1, 'yhat'] > forecast.loc[i, 'yhat']:
+      forecast.loc[i, 'yhat'] = forecast.loc[i - 1, 'yhat']
+  forecast['old'] = selected_df_train[label] # 'old' is old label
+  for i in range(len(forecast)):
+    if not math.isnan(forecast.loc[i, 'old']):
+      forecast.loc[i, label] = forecast.loc[i, 'old']
+    else:
+      forecast.loc[i, label] = forecast.loc[i - 1, label] + forecast.loc[i, 'yhat']
+
+  forecast = forecast[['ds', label]]
+  forecast = forecast.round({label: 0})
   forecast.rename(columns={'ds': 'Date'}, inplace=True)
   forecast = forecast.set_index('Date')
-
+  # print_all_rows(forecast)
   return forecast
-
-def get_resonable_value(row):
-  if math.isnan(row['y']):
-    return row['yhat']
-  return row['y']
 
 def rmsle(actual_value, predicted_value):
   '''
@@ -46,7 +63,7 @@ if __name__ == "__main__":
   submission_df = pd.DataFrame(columns=['ForecastId', 'ConfirmedCases', 'Fatalities'])
 
   # Read test file
-  df_test = pd.read_csv('test.csv')
+  df_test = pd.read_csv(DATA_FOLDER + 'test.csv')
   df_test['Date'] = pd.to_datetime(df_test['Date'], format='%Y-%m-%d')
 
   # Read data to train
@@ -62,15 +79,14 @@ if __name__ == "__main__":
 
     forecast_case = prophet_calculation(df_train_region, "ConfirmedCases")
     forecast_fatalities = prophet_calculation(df_train_region, "Fatalities")
-    df_test_region['ConfirmedCases'] = forecast_case.apply(get_resonable_value, axis=1)
-    df_test_region['Fatalities'] = forecast_fatalities.apply(get_resonable_value, axis=1)
-    print(df_test_region)
+    df_test_region['ConfirmedCases'] = forecast_case['ConfirmedCases']
+    df_test_region['Fatalities'] = forecast_fatalities['Fatalities']
     df_test_region = df_test_region[['ForecastId', 'ConfirmedCases', 'Fatalities']]
-    # df_test_region = df_test_region.set_index('ForecastId')
     df_test_region['ConfirmedCases'] = df_test_region['ConfirmedCases'].astype(np.int64)
     df_test_region['Fatalities'] = df_test_region['Fatalities'].astype(np.int64)
+    # print_all_rows(df_test_region)
     submission_df = submission_df.append(df_test_region, ignore_index=True)
-    if count == 0:
-      break
-    count += 1
+    # if count == 1:
+    #   break
+    # count += 1
   submission_df.to_csv('submission.csv', index=False)
